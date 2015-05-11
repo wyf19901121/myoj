64ffb7e92c84fb0d83c7c0e84f6b0e2a46a1bbb2
wyf19901121 <wyf19901121@163.com>
2015-05-12 00:25:33 +0800

    add FindMinimuminRotatedSortedArray

---
+++ src/leetcode/FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class FindMinimuminRotatedSortedArrayII {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+        	if (nums[low] < nums[high]) {
+				return nums[low];
+			}
+        	int mid = low + (high - low) / 2;
+        	if (nums[mid] == nums[low]) {
+				if (low + 1 == high) {
+					low = high;
+					break;
+				}
+				low++;
+			}
+        	else if (nums[mid] < nums[high]) {
+				high = mid;
+			}
+        	else {
+				low = mid;
+			}
+		}
+        return nums[low];
+    }
+}
