8fb522abc62ca99bd7574549bbcab32d1069b194
wyf19901121 <649702854@qq.com>
2015-06-05 16:49:40 +0800

    modify ShortestPalindrome

--- src/leetcode/ShortestPalindrome.java
+++ src/leetcode/ShortestPalindrome.java
@@ -7,17 +7,18 @@ public class ShortestPalindrome {
 			return "";
 		}
         String r = s + new StringBuilder(s).reverse().toString();
-        int[] next = new int [s.length() * 2];
-        for (int i = 1; i < next.length; i++) {
-			int j = next[i - 1];
+        int[] next = new int [s.length() * 2 + 1];
+        next[0] = next[1] = 0;
+        for (int i = 1; i < next.length - 1; i++) {
+			int j = next[i];
 			while (r.charAt(i) != r.charAt(j) && j > 0) {
-				j = next[j - 1];
+				j = next[j];
 			}
 			if (r.charAt(i) == r.charAt(j)) {
-				next[i] = j + 1;
+				next[i + 1] = j + 1;
 			}
 			else {
-				next[i] = 0;
+				next[i + 1] = 0;
 			}
 		}
         int count = s.length() - next[next.length - 1];
