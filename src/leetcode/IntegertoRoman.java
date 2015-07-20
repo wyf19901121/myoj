27b5403daaa9c918825527318a55317a650de72f
wyf19901121 <649702854@qq.com>
2015-07-20 14:09:58 +0800

    add IntegertoRoman

--- src/leetcode/IntegertoRoman.java
+++ src/leetcode/IntegertoRoman.java
@@ -4,32 +4,36 @@ public class IntegertoRoman {
     private int Num[] = {1, 5, 10, 50, 100, 500, 1000};
     private char Roman[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     public String intToRoman(int num) {
-        String reString = "";
-        for (int i = Num.length - 1; i >= 0; i--) {
-            switch (num/Num[i]) {
-            case 1:
-                if (i == 0 || num/Num[i-1] != 9) {
-                    reString += String.valueOf(Roman[i]);
-                }
-                else {
-                    reString += (String.valueOf(Roman[i-1]) + String.valueOf(Roman[i+1]));
-                    i--;
-                }
-                break;
-            case 2:
-                reString += (String.valueOf(Roman[i]) + String.valueOf(Roman[i]));
-                break;
-            case 3:
-                reString += (String.valueOf(Roman[i]) + String.valueOf(Roman[i]) + String.valueOf(Roman[i]));
-                break;
-            case 4:
-                reString += (String.valueOf(Roman[i]) + String.valueOf(Roman[i+1]));
-                break;
-            default:
-                break;
-            }
-            num = num - num/Num[i]*Num[i];
-        }
-        return reString;
+    	StringBuilder reStringBuilder = new StringBuilder();
+    	for (int i = Num.length - 1; i >= 0; i--) {
+			switch (num/Num[i]) {
+			case 1:
+				if (i == 0 || num/Num[i-1] != 9) {
+					reStringBuilder.append(Roman[i]);
+				}
+				else {
+					reStringBuilder.append(Roman[i-1]);
+					reStringBuilder.append(Roman[i+1]);
+					i--;
+				}
+				break;
+			case 2:
+				reStringBuilder.append(Roman[i]);
+				reStringBuilder.append(Roman[i]);
+				break;
+			case 3:
+				reStringBuilder.append(Roman[i]);
+				reStringBuilder.append(Roman[i]);
+				reStringBuilder.append(Roman[i]);
+				break;
+			case 4:
+				reStringBuilder.append(Roman[i]);
+				reStringBuilder.append(Roman[i+1]);
+			default:
+				break;
+			}
+			num = num - num/Num[i]*Num[i];
+		}
+    	return reStringBuilder.toString();
     }
 }
