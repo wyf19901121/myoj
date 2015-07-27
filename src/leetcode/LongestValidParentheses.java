af9cb5803fdb507389f285cca90caa06466acd27
wyf19901121 <wyf19901121@163.com>
2015-07-27 23:53:55 +0800

    update LongestValidParentheses

--- src/leetcode/LongestValidParentheses.java
+++ src/leetcode/LongestValidParentheses.java
@@ -4,27 +4,28 @@ import java.util.Stack;
 
 public class LongestValidParentheses {
     public int longestValidParentheses(String s) {
-        boolean[] match = new boolean[s.length()];
-        Stack<Integer> index = new Stack<>();
-        for (int i = 0; i < match.length; i++) {
-            if (s.charAt(i) == '(') {
-                index.push(i);
-            }
-            else if (!index.empty()) {
-                match[i] = true;
-                match[index.pop()] = true;
-            }
-        }
-        int maxlen = 0, curlen = 0;
-        for (int i = 0; i < match.length; i++) {
-            if (match[i]) {
-                curlen++;
-            }
-            else {
-                curlen = 0;
-            }
-            maxlen = Math.max(maxlen, curlen);
-        }
-        return maxlen;
+    	boolean[] match = new boolean[s.length()];
+    	Stack<Integer> indexs = new Stack<Integer>();
+    	for (int i = 0; i < match.length; i++) {
+			char ch = s.charAt(i);
+			if (ch == '(') {
+				indexs.push(i);
+			}
+			else if (!indexs.isEmpty()) {
+				match[i] = true;
+				match[indexs.pop()] = true;
+			}
+		}
+    	int len = 0, max = 0;
+    	for (int i = 0; i < match.length; i++) {
+			if (match[i] == true) {
+				len++;
+			}
+			else {
+				len = 0;
+			}
+			max = Math.max(len, max);
+		}
+    	return max;
     }
 }
