f1c83130f64c30a93ff58bb037f1091e83307c23
wyf19901121 <wyf19901121@163.com>
2015-08-19 00:55:07 +0800

    add DeleteNodeinaLinkedList

---
+++ src/leetcode/DeleteNodeinaLinkedList.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class DeleteNodeinaLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
