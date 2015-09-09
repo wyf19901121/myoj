caef4429c7736466d99d2dc17926b31f7bc3313b
wyf19901121 <wyf19901121@163.com>
2015-09-09 23:41:02 +0800

    update BuyBook14

--- src/codebeauty/BuyBook14.java
+++ src/codebeauty/BuyBook14.java
@@ -1,33 +1,46 @@
 package codebeauty;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BuyBook14 {
     private double price = 8.00;
     private int[] books = {1, 1, 1, 1, 1};
     private double[] discout = {0, 1.00, 0.95, 0.90, 0.80, 0.75};
     
     public double solution() {
-        if (books[0] == 0 && books[1] == 0 && books[2] == 0
-                && books[3] == 0 && books[4] == 0) {
-            return 0.00;
-        }
-        List<Double> curPrice = new ArrayList<>();
-        int count = 1;
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] != 0) {
-                books[i]--;
-                curPrice.add(count++*price*discout[count-1] + solution());
+        double[][][][][] res = new double[books[0]+1][books[1]+1][books[2]+1][books[3]+1][books[4]+1];
+        res[0][0][0][0][0] = 0.00;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j <= books[i]; j++) {
+                switch (i) {
+                case 0:
+                    res[j][0][0][0][0] = price + res[j-1][0][0][0][0];
+                    break;
+                case 1:
+                    res[books[0]][j][0][0][0] = Math.min(price + res[books[0]][j-1][0][0][0], price * 2 * discout[2] + res[books[0]-1][j-1][0][0][0]);
+                    break;
+                case 2:
+                    res[books[0]][books[1]][j][0][0] = Math.min(Math.min(price + res[books[0]][books[1]][j-1][0][0], 
+                            price * 2 * discout[2] + res[books[0]][books[1]-1][j-1][0][0]),
+                            price * 3 * discout[3] + res[books[0]-1][books[1]-1][j-1][0][0]);
+                    break;
+                case 3:
+                    res[books[0]][books[1]][books[2]][j][0] = Math.min(Math.min(Math.min(price + res[books[0]][books[1]][books[2]][j-1][0], 
+                            price * 2 * discout[2] + res[books[0]][books[1]][books[2]-1][j-1][0]),
+                            price * 3 * discout[3] + res[books[0]][books[1]-1][books[2]-1][j-1][0]), 
+                            price * 4 * discout[4] + res[books[0]-1][books[1]-1][books[2]-1][j-1][0]);
+                    break;
+                case 4:
+                    res[books[0]][books[1]][books[2]][books[3]][j] = Math.min(Math.min(Math.min(Math.min(price + res[books[0]][books[1]][books[2]][books[3]][j-1], 
+                            price * 2 * discout[2] + res[books[0]][books[1]][books[2]][books[3]-1][j-1]), 
+                            price * 3 * discout[3] + res[books[0]][books[1]][books[2]-1][books[3]-1][j-1]), 
+                            price * 4 * discout[4] + res[books[0]][books[1]-1][books[2]-1][books[3]-1][j-1]), 
+                            price * 5 * discout[5] + res[books[0]-1][books[1]-1][books[2]-1][books[3]-1][j-1]);
+                    break;
+                default:
+                    break;
+                }
             }
         }
-        double min = Double.MAX_VALUE;
-        for (int i = 0; i < curPrice.size(); i++) {
-            if (curPrice.get(i) < min) {
-                min = curPrice.get(i);
-            }
-        }
-        return min;
+        return res[books[0]][books[1]][books[2]][books[3]][books[4]];
     }
     
     public static void main(String[] args) {
