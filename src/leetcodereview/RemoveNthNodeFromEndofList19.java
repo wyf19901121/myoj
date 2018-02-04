6fe1ac755627aa0968d03ae9c472d4872018c70d
wyf19901121 <wyf19901121@163.com>
2018-02-04 23:27:57 +0800

    IDEA add leetcodereview/add 19

---
+++ src/leetcodereview/RemoveNthNodeFromEndofList19.java
@@ -0,0 +1,28 @@
+
+/**
+ * Created by wangyifan on 2018/2/4.
+ */
+public class RemoveNthNodeFromEndofList19 {
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
