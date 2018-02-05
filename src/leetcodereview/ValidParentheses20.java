c33fa42e0118a3c3ea25bda65e156d8090e89a4b
wyf19901121 <wyf19901121@163.com>
2018-02-05 22:44:33 +0800

    IDEA add leetcodereview/add 20

---
+++ src/leetcodereview/ValidParentheses20.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+/**
+ * Created by wangyifan on 2018/2/5.
+ */
+public class ValidParentheses20 {
+    public boolean isValid(String s) {
+        if (s == null || s.length() % 2 == 1) {
+            return false;
+        }
+
+        char[] ch = s.toCharArray();
+        Stack<Character> charStack = new Stack<>();
+
+        for (int i = 0; i < ch.length; i++) {
+            switch (ch[i]) {
+                case '(':
+                case '[':
+                case '{':
+                    charStack.push(ch[i]);
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                    if (charStack.empty()) {
+                        return false;
+                    }
+                    char topChar = charStack.pop();
+                    if (topChar == '(' && ch[i] == ')'
+                            || topChar == '[' && ch[i] == ']'
+                            || topChar == '{' && ch[i] == '}') {
+                    }
+                    else {
+                        return false;
+                    }
+            }
+        }
+        return charStack.empty();
+    }
+}
